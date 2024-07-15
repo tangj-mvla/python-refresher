2fc32a869f7333e538fbf003ab084cce10cec0d1
tangj-mvla <joshuatang2007@gmail.com>
2024-07-15 08:20:09 -0400

    added to physics

--- physics.py
+++ physics.py
@@ -30,3 +30,36 @@ def calculate_auv_angular_acceleration(F_magnitude, F_angle, inertia = 1, thrust
     return calculate_angular_acceleration(tau, inertia)
 
 def calculate_auv2_acceleration(T, alpha, theta, mass = 100):
+    a1 = calculate_acceleration(T[0],alpha,mass)
+    a2 = calculate_acceleration(T[1],alpha,mass)
+    a3 = calculate_acceleration(T[2],alpha,mass)
+    a4 = calculate_acceleration(T[3],alpha,mass)
+    return a1 + a2 - a3 - a4
+
+def calculate_auv2_angular_acceleration(T, alpha, L, l, inertia = 100):
+    distance = np.sqrt(L**2, l**2)
+    t1 = distance * T[0] * np.sin(alpha)
+    t2 = distance * T[1] * np.sin(alpha)
+    t3 = distance * T[2] * np.sin(alpha)
+    t4 = distance * T[3] * np.sin(alpha)
+    return (t1 + t3 - t2 -t4)/inertia
+    
+def simulate_auv2_motion(T, alpha, L, l, mass = 100, inertia = 100, dt = 0.1, t_final = 10, x0 = 0, y0 = 0, theta0 = 0):
+    alpha = math.pi/180 * alpha
+    t = np.arange(0,t_final+dt,dt)
+    moment_arm = np.sqrt(l**2+L**2)
+    net_torque = moment_arm*np.sin(alpha)*(T[0]+T[2]-T[1]-T[3])
+    theta = t**2 + 1/2 * net_torque/inertia + theta0
+    print (theta)
+
+    horizontal = np.sin(math.pi/2-alpha+theta)*(T[0] + T[1] - T[2] - T[3])/mass
+    vertical = np.cos(math.pi/2-alpha+theta)*(T[0] + T[1] - T[2] - T[3])/mass
+    a = np.sqrt(horizontal**2 + vertical**2)
+
+    hv = np.array([sum(horizontal[:1])]) * dt for i in range(len(horizontal))
+    vv = np.array([sum(vertical[:1])]) * dt for i in rnage(len(vertical))
+    v = np.sqrt(hv**2 + vv**2)
+
+    x = x0 + np.array([sum(hv[:1])]) * dt for i in range(len(hv))
+    y = y0 + np.array([sum(vv[:1])]) * dt for i in rnage(len(vv))
+    
